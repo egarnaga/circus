7d973982b4820c9bd98749bc4c8384eacddbd61e
Evgeniia Garnaga <evgarnaga@gmail.com>
2021-04-19 01:02:22 +0600

    добавляет smooth scroll

--- source/js/main.js
+++ source/js/main.js
@@ -2,9 +2,11 @@ import {ieFix} from './utils/ie-fix';
 
 import {initModals} from './modules/init-modals';
 
+import {smoothScroll} from './modules/smoothScroll';
 import {openMenu} from './modules/openMenu';
 import {sliderVertical} from './modules/sliderVertical';
 import {sliderHorizontal} from './modules/sliderHorizontal';
+
 
 // Utils
 // ---------------------------------
@@ -15,6 +17,9 @@ ieFix();
 // ---------------------------------
 
 initModals();
+
+smoothScroll();
 openMenu();
 sliderVertical();
 sliderHorizontal();
+
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,1 +1,2 @@
 import './vendor/swiper';
+import './vendor/jquery';
